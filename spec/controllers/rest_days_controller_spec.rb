e15ed8bfc16ce665bdb8ddbfcadb35ddd9f3d253
Yoshihiro Imaida <26681755+y-imaida@users.noreply.github.com>
2018-04-27 16:29:41 +0900

    CSVインポートに関するテストを追加

--- spec/controllers/rest_days_controller_spec.rb
+++ spec/controllers/rest_days_controller_spec.rb
@@ -14,4 +14,41 @@ describe RestDaysController, type: :controller do
       end
     end
   end
+
+  describe 'POST import' do
+    before { login_user }
+
+    context 'CSVインポートが成功する場合' do
+      let(:import_file) { File.expand_path('../../fixtures/rest_days.csv', __FILE__) }
+      let(:params) { { rest_day_csv: { file: create_uploaded_file(import_file, 'text/csv') } } }
+      subject { post :import, params }
+
+      it 'RestDayが4増加' do
+        subject
+        expect(RestDay.count).to eq(4)
+      end
+    end
+
+    context 'CSVインポートが失敗する場合' do
+      context '入力ファイルの拡張子がcsv以外の場合' do
+        render_views
+
+        let(:import_file) { File.expand_path('../../fixtures/rest_days.txt', __FILE__) }
+        let(:params) { { rest_day_csv: { file: create_uploaded_file(import_file, 'text/plain') } } }
+        subject { post :import, params }
+
+        it 'RestDayが登録されない' do
+          subject
+          expect(RestDay.count).to eq(0)
+        end
+
+        it 'フラッシュメッセージを含んだindex画面を表示する' do
+          subject
+          expect(response).to render_template(:index)
+          expect(response).to be_success
+          expect(response.body).to have_selector '#errorExplanation'
+        end
+      end
+    end
+  end
 end
--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -3,4 +3,12 @@ module ControllerHelpers
     session[:user_id] = user.id
     session[:ctime] = Time.now.tomorrow.utc.to_i
   end
+
+  def create_uploaded_file(file, mime_type)
+    ActionDispatch::Http::UploadedFile.new(
+      filename: File.basename(file),
+      type: mime_type,
+      tempfile: File.open(file)
+    )
+  end
 end
