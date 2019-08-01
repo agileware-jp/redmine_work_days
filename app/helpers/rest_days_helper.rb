57df117b6461ae7908f8fcce7f970de6f4f601ce
Seiei Miyagi <hanachin@gmail.com>
2019-08-01 17:45:16 +0900

    jQuery.fn.datepickerFallbackが定義されているかガードしフォールバック定義のJSを1度しか吐かないようにした

--- app/helpers/rest_days_helper.rb
+++ app/helpers/rest_days_helper.rb
@@ -5,34 +5,39 @@ module RestDaysHelper
     end
 
     include_calendar_headers_tags
-    content_for(:header_tags) do
-      javascript_tag(<<-JS.strip_heredoc)
-        // backport of https://redmine.org/projects/redmine/repository/revisions/15375
-        (function ( $ ) {
-          // detect if native date input is supported
-          var nativeDateInputSupported = true;
+    unless @calendar_for_with_datepicker_fallback
+      @calendar_for_with_datepicker_fallback = true
+      content_for(:header_tags) do
+        javascript_tag(<<-JS.strip_heredoc)
+          if (!jQuery.fn.datepickerFallback) {
+            // backport of https://redmine.org/projects/redmine/repository/revisions/15375
+            (function ( $ ) {
+              // detect if native date input is supported
+              var nativeDateInputSupported = true;
 
-          var input = document.createElement('input');
-          input.setAttribute('type','date');
-          if (input.type === 'text') {
-            nativeDateInputSupported = false;
+              var input = document.createElement('input');
+              input.setAttribute('type','date');
+              if (input.type === 'text') {
+                nativeDateInputSupported = false;
+              }
+
+              var notADateValue = 'not-a-date';
+              input.setAttribute('value', notADateValue);
+              if (input.value === notADateValue) {
+                nativeDateInputSupported = false;
+              }
+
+              $.fn.datepickerFallback = function( options ) {
+                if (nativeDateInputSupported) {
+                  return this;
+                } else {
+                  return this.datepicker( options );
+                }
+              };
+            }( jQuery ));
           }
-
-          var notADateValue = 'not-a-date';
-          input.setAttribute('value', notADateValue);
-          if (input.value === notADateValue) {
-            nativeDateInputSupported = false;
-          }
-
-          $.fn.datepickerFallback = function( options ) {
-            if (nativeDateInputSupported) {
-              return this;
-            } else {
-              return this.datepicker( options );
-            }
-          };
-        }( jQuery ));
-      JS
+        JS
+      end
     end
     javascript_tag("$(function() { $('##{field_id}').addClass('date').datepickerFallback(datepickerOptions); });")
   end
